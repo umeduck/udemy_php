9305bcd6a01186f1eb3aa7331c9222e8027c1532
umeduck <ume_duck@outlook.jp>
2023-02-12 22:33:29 +0900

    コンフリクトの修正

--- mainte/test.php
+++ mainte/test.php
@@ -1,15 +1,4 @@
 <?php
-
-<<<<<<< HEAD
-// パスワードを記録したファイル場所
-echo __FILE__;
-// /Applications/MAMP/htdocs/php_test/MAINTE/TEST.PHP
-echo '<br>';
-// パスワード暗号化
-echo(password_hash('password123',PASSWORD_BCRYPT));
-// $2y$10$9LfjJlxjeyzWIOz3OEXtVuW7VKXIGe4FgX/EhHjNrmaQUCKqZj0Ce
-
-=======
 // ファイル名型
 
 // // ファイル名を変数に
@@ -52,4 +41,3 @@ $addText = '1行追記' . "\n";
 fwrite($contents, $addText);
 
 fclose($contents);
->>>>>>> master
