8ade485d5d2f3da972c6b4e61a180b48757b7e85
umeduck <masa.0116>
2023-02-18 16:01:04 +0900

    継承クラスの記述

---
+++ function/object/extends.php
@@ -0,0 +1,72 @@
+<?php
+// 親クラス
+class BaseProduct{
+  // 変数関数
+  public function echoProduct(){
+    echo '親クラスです';
+  }
+
+  //オーバーライド
+  public function getName(){
+    echo '親のクラスです';
+  }
+}
+
+// 子クラス
+class Bird extends BaseProduct {
+  //アクセス修飾子、private(外からはアクセスできない)、protected(自分のクラス・継承したクラスのみ使用できる)、public(外からアクセスできる(default))
+
+  //変数
+  private $name = [];
+  private $text = [];
+  //メソッド
+  function __construct($name){
+    $this->name = $name;
+  }
+
+  // public function getName(){
+  //   echo $this->name;
+  // }
+
+  public function addText(){
+    $this->text = '僕は'.$this->name.'です';
+  }
+
+  public function getText(){
+    echo $this->text;
+  }
+
+  public static function addInt($a, $b){
+    return $a+$b;
+  }
+}
+
+$bird = new Bird('だいふく');
+
+//親クラスのメソッド
+$bird->echoProduct();
+
+$bird->getName();
+$bird->addText();
+$bird->getText();
+
+$int = Bird::addInt(9,1478);
+echo $int;
+
+
+class Sample{
+  public function getWeather(){
+    echo "明日は晴れ";
+  }
+  public static function getNews(){
+      echo 'コロナが流行しました';
+  }
+}
+$sample = new Sample();
+$sample->getWeather();
+Sample::getNews();
+
+
+// オーバーライド
+// 親クラスと子クラスに同じメソッド名があった際子クラスからそのメソッドが呼び出せれるとこうラスのメソッドを優先される
+
